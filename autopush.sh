ecd52f770873dddb1544ead1d6c7f5b63d78ad0e
kurniar <kmoharif@gmail.com>
2026-02-16 18:01:24 +0700

    Update autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Cek apakah ini folder Git
+if [ ! -d ".git" ]; then
+	echo "Ini bukan folder Git! Jalankan 'git init' dulu."
+	exit 1
+fi
+
+# Cek apakah ada file .gitignore
+if [ ! -f ".gitignore" ]; then
+	echo "Peringatan: .gitignore tidak ditemukan!"
+	echo "Apaka kamu tetap ingin melakukan push tanpa perlindungan? (y/n)"
+	read JAWABAN
+
+	if [ "$JAWABAN" != "y" ]; then
+		echo "Push dibatalkan. Silahkan buat .gitignore dulu"
+		exit 1
+	fi
+fi
+
 # Minta pesan commit
 echo "Masukkan pesan commit"
 read PESAN
@@ -7,10 +25,6 @@ read PESAN
 # Jalankan Git
 git add .
 git commit -m "$PESAN"
-
 echo "Sedang mengirim ke GitHub..."
-
 git push origin master
-
-echo "Selesai! Tugasmu aman di GitHub."
-
+echo "Selesai! Tugasmu aman di GitHub. dengan pesan : '$PESAN'"
